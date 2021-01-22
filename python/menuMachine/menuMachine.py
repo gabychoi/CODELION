c32e85b7868563d20ffe06df43308d881ebea11d
gabychoi <hojin.choi.dev@gmail.com>
2021-01-22 10:30:40 +0900

    menuMachine

--- python/menuMachine/menuMachine.py
+++ python/menuMachine/menuMachine.py
@@ -1,5 +1,12 @@
 lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
 while True:
     item = input("음식을 추가해주세요 : ")
-    lunch.append(item)
-    print(lunch)
+    if(item == 'q'):
+        break
+    else: 
+        lunch.append(item)
+
+print(lunch)
+
+#집합으로 변경
+set_lunch = set(lunch)
