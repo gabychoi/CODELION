9dc6aa6c6538620bcba863470ff1827cbbed7494
gabychoi <hojin.choi.dev@gmail.com>
2021-01-25 15:16:14 +0900

    questionBoard

--- python/python_basic.py
+++ python/python_basic.py
@@ -109,6 +109,20 @@ else:
     print("그냥 주세요.")
 print('종료')
 
+#if ~elif ~else문
+season = input("계절을 입력하세요 :")
+
+if season == "봄" :
+	print("가디건")
+elif season == "여름" :
+	print("반팔")
+elif season == "가을" :
+	print("코트")
+elif season == "겨울" :
+	print("패딩")
+else :
+    print("잘못 입력하셨습니다.")
+
 #함수
 def make_dolcelatte():
     print("1. 얼음을 넣는다.")
